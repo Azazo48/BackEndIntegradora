1c86954adbc6d62d15a89910a69981fb36c8c256
Hassiel <hassielcuadrasraygoza@gmail.com>
2025-02-27 12:28:29 -0700

    si7

--- app.js
+++ app.js
@@ -200,7 +200,7 @@ app.post("/servicios", async (req, res) => {
     const { empresa, nombre, descripcion, precio, duracion } = req.body;
 
     try {
-        await agregarServicio({ empresa, nombre, descripcion, precio, duracion });
+        await agregarServicio( empresa, nombre, descripcion, precio, duracion );
         res.status(201).json({ message: "Servicio agregado exitosamente." });
     } catch (error) {
         res.status(500).json({ error: "No se pudo agregar el servicio." });
