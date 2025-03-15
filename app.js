9a2a6b7fa18a184e500b191b3dffc8e2948cd3c1
Hassiel <hassielcuadrasraygoza@gmail.com>
2025-03-14 18:09:03 -0700

    Comit para sulucionar lo de las imagenes18

--- app.js
+++ app.js
@@ -46,14 +46,10 @@ app.use(cors());
 const storage = multer.memoryStorage();
 const upload = multer({
     storage: storage,
-    fileFilter: (req, file, cb) => {
-      if (!file.mimetype.startsWith('image/')) {
-        return cb(new Error('Solo se permiten imágenes'), false);
-      }
-      cb(null, true);
-    }
-});
-  
+    limits: {
+      fileSize: 5 * 800 * 800, // Límite de 50 MB
+    },
+  });
 
 
 
