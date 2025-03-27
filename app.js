2b1394ad6d22627735157587f808a968da8a7504
Hassiel <hassielcuadrasraygoza@gmail.com>
2025-03-27 11:39:50 -0700

    Push para horarios14

--- app.js
+++ app.js
@@ -59,14 +59,14 @@ const upload = multer({
 app.get("/VerHorarios", async (req, res) => {
     try {
         const { fecha } = req.body;
-        const horario = await VerHorarios(fecha);
+        const [hora] = await VerHorarios(fecha);
         const horarios = [
             "10:00", "10:30", "11:00", "11:30", "12:00", "12:30",
             "13:00", "13:30", "14:00", "14:30", "15:00", "15:30",
             "16:00", "16:30", "17:00", "17:30", "18:00",
         ];
 
-        console.log("horarios",horario)
+        console.log("horarios",hora)
         console.log(horarios)
 
         //const horas = horario.map(item => item.hora.slice(0, 5));
