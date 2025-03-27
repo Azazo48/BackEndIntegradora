96a7d51131522b57155a6d1edd544e3c9840bbb0
Hassiel <hassielcuadrasraygoza@gmail.com>
2025-03-27 11:17:37 -0700

    Push para horarios5

--- app.js
+++ app.js
@@ -65,11 +65,14 @@ app.get("/VerHorarios", async (req, res) => {
             "13:00", "13:30", "14:00", "14:30", "15:00", "15:30",
             "16:00", "16:30", "17:00", "17:30", "18:00",
         ];
+
         console.log(horario)
         console.log(horarios)
+
         const resultado1 = horario.filter(item => !horarios.includes(item));
+
         console.log(resultado1)
-        res.status(200).json(horario);
+        res.status(200).json(resultado1);
     } catch (error) {
         res.status(500).json({ error: "No se pudieron obtener los horarios" });
     }
